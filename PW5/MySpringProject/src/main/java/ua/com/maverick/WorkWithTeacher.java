c5ea874cab84850c3a6a51a2600d6b7310635fdd
maxmakuha <maxmakuha@gmail.com>
2015-03-15 15:00:06 +0200

    #5
    Практичне 5. Робота з базами дани�

---
+++ PW5/MySpringProject/src/main/java/ua/com/maverick/WorkWithTeacher.java
@@ -0,0 +1,27 @@
+package ua.com.maverick;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import ua.com.maverick.TeachersDao;
+import ua.com.maverick.Teacher;
+
+@Transactional
+public class WorkWithTeacher {
+	@Autowired
+	private TeachersDao teachersDao;
+
+	public Teacher addTeacher(Teacher teacher) {
+		teachersDao.addTeacher(teacher);
+		System.out.println("Teacher has been added " + teacher);
+		return teacher;
+	}
+
+	public Teacher getTeacherById(int id) {
+		return teachersDao.getTeacherById(id);
+	}
+
+	public void saveTacher(Teacher teacher) {
+		teachersDao.saveTacher(teacher);
+		System.out.println("Teacher has been saved " + teacher);
+	}
+}
